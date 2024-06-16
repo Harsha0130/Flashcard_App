889e8be9194f3357d1f754e0e9b6b28d645cc538
Harsha <harshahm0130@gmail.com>
2024-06-16 23:20:41 +0530

    Flipping flash cards

--- main.py
+++ main.py
@@ -4,21 +4,28 @@ import random
 
 BACKGROUND_COLOR = "#70d8e3"
 
+
 # ---------------------------- CREATING NEW FLASH CARDS ------------------------------- #
 data = pandas.read_csv("data/kannada_words.csv")
 to_learn = data.to_dict(orient="records")
-print(to_learn)
-
-
-def start():
-    next_card()
-    start_button.destroy()
+current_card = {}
 
 
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    canvas.itemconfig(language_text, text="Kannada")
-    canvas.itemconfig(word_text, text=current_card["Kannada"])
+    canvas.itemconfig(language_text, text="Kannada", fill="black")
+    canvas.itemconfig(word_text, text=current_card["Kannada"], fill="black")
+    canvas.itemconfig(card_bg, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+# ---------------------------- FLIPPING THE CARD ------------------------------- #
+def flip_card():
+    canvas.itemconfig(language_text, text="English", fill="white")
+    canvas.itemconfig(word_text, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_bg, image=card_back_img)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -26,9 +33,12 @@ window = Tk()
 window.title("Flashh")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
 canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
+card_back_img = PhotoImage(file="images/card_back.png")
+card_bg = canvas.create_image(400, 263, image=card_front_img)
 language_text = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
 word_text = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
@@ -43,8 +53,6 @@ no_button = Button(image=no_image, highlightthickness=0, bg=BACKGROUND_COLOR, ac
                    command=next_card)
 no_button.grid(column=1, row=1, padx=20, pady=20)
 
-start_button = Button(text="Start", highlightthickness=0, width=15, activebackground=BACKGROUND_COLOR, height=5,
-                      font=("Ariel", 30, "bold"), command=start)
-start_button.grid(column=0, row=0, columnspan=2)
+next_card()
 
 window.mainloop()
